74da99606dd731267a2422ca5c81cba134108aaa
Wilson Chen <wilsonchen901@gmail.com>
2023-01-24 14:47:34 -0500

    edit base

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
   resolve: {
     alias: [{find: "@", replacement: path.resolve(__dirname, "src")}]
   },
-  base: '/project-movement'
+  base: '/project-movement/'
 })
