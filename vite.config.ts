d3df735d2db451ce2cb5c5d6568a8d7d9dd6fe7b
Wilson Chen <wilsonchen901@gmail.com>
2023-01-24 14:58:10 -0500

    redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project-movement/',
   plugins: [react()],
   resolve: {
     alias: [{find: "@", replacement: path.resolve(__dirname, "src")}]
   },
-  base: '/project-movement/'
 })
